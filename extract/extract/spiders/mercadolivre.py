db94a0b54bead52a093fd8ca65b799e27de827c0
Andrews Goes <andrews.goes.33@gmail.com>
2024-12-18 08:54:08 -0300

    feat: extract de 10 pag. completo

--- extract/extract/spiders/mercadolivre.py
+++ extract/extract/spiders/mercadolivre.py
@@ -5,7 +5,9 @@ class MercadolivreSpider(scrapy.Spider):
     name = "mercadolivre"
     allowed_domains = ["lista.mercadolivre.com.br"]
     start_urls = ["https://lista.mercadolivre.com.br/notebooks"]
-
+    page_count = 1
+    max_pages = 10
+    
     def parse(self, response):
         
         products = response.css('div.poly-card__content')
@@ -18,9 +20,14 @@ class MercadolivreSpider(scrapy.Spider):
                    'name' : product.css('h2.poly-box.poly-component__title a::text').get(),
                    'seller' : product.css('span.poly-component__seller::text').get(),
                    'old_price' : prices[0] if len(prices) > 0 else None,
-                   'new_price' : prices[1] if len(prices) > 0 else None,
+                   'new_price' : prices[1] if len(prices) > 1 else None,
                    'reviews_rating' : product.css('span.poly-reviews__rating::text').get(),
                    'reviews_total' : product.css('span.poly-reviews__total::text').get()
             
             }
-        
+
+        if self.page_count < self.max_pages:
+            next_page = response.css('li.andes-pagination__button.andes-pagination__button--next a::attr(href)').get()
+            if next_page:
+                self.page_count += 1
+                yield scrapy.Request(url=next_page, callback=self.parse)
